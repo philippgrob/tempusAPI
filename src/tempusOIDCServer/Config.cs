c06bfe249fdde4d6883f39a03bb7704906a0aa7f
Philipp <Philipp@code>
2017-04-22 18:48:16 +0200

    merged oidc to tempusAPI

---
+++ src/tempusOIDCServer/Config.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace tempusOIDCServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApitResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {"api1"}
+                },
+
+                // resource owner password grant client
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {"api1"}
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser()
+                {
+                    SubjectId = "1",
+                    Username = "ckelley0",
+                    Password = "password"
+                },
+                new TestUser()
+                {
+                    SubjectId = "2",
+                    Username = "cbishop2",
+                    Password = "password2"
+                }
+            };
+        }
+    }
+}
